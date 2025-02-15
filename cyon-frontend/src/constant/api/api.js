5938feb65058d388aa02f4ff70fb401ebbfad94a
olajirestephen <horlharmighty2000@gmail.com>
2025-02-15 06:58:09 -0600

    added forward slash to root url

--- cyon-frontend/src/constant/api/api.js
+++ cyon-frontend/src/constant/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {jwtDecode} from 'jwt-decode';
 
 const link = "http://127.0.0.1:8000/api/"
-const render = "https://cyon-backend.onrender.com";
+const render = "https://cyon-backend.onrender.com/api/";
 
 const api = axios.create({
   baseURL: render,
